dc250ee8025551f8682d8f47c76051abaceb02a2
malachek <malachykennedy1@gmail.com>
2023-11-19 15:06:53 -0800

    player controller

--- GDIM31 Project/Assets/Scripts/PlayerController.cs
+++ GDIM31 Project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     [SerializeField] public float speed;
 
@@ -28,11 +29,49 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         
+=======
+    public float speed;
+    public float jumpForce;
+    float moveInput;
+
+    Rigidbody2D rb;
+
+    bool facingRight = true;
+
+    bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(!facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight && moveInput < 0)
+        {
+            Flip();
+        }
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
         if(isGrounded && Input.GetKeyDown(KeyCode.Space))
@@ -66,5 +105,19 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+=======
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+>>>>>>> Stashed changes
     }
 }
