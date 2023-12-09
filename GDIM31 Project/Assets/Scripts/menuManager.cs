8ef8321583592d6dcf651a8b868849cf7d722f55
malachek <malachykennedy1@gmail.com>
2023-12-08 23:31:25 -0800

    Update menuManager.cs

--- GDIM31 Project/Assets/Scripts/menuManager.cs
+++ GDIM31 Project/Assets/Scripts/menuManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class menuManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     public PauseMenu pauseMenu;
 
